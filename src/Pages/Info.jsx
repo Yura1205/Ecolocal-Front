62f943538528a0db42107f4f95b7c7ac12c31c58
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2023-11-10 05:30:28 -0500

    Styles Fixing and Info Update

--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -7,6 +7,29 @@ const stylesInfo = {
     marginTop: "10vh",
     maxWidth: "100wh",
   },
+  imgInfo: {
+    marginTop: "5vh",
+    marginLeft: "2vw",
+  },
+  tittle1: {
+    textAlign: "center",
+
+  },
+  infoContent: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  infoText: {
+    display: "box",
+    height: "50%",
+    width: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "6% 5% 5% 5%",
+    
+  },
 }
 
 export const Info = () => {
@@ -14,7 +37,15 @@ export const Info = () => {
     <>
       <DashboardNav />
       <div className="infoMain" style={stylesInfo.infoMain}>
-        <div>Info</div>
+        <h1 className='tittle1' style={stylesInfo.tittle1}>Bandeja Paisa</h1>
+        <div className='infoContent' style={stylesInfo.infoContent}>
+          <div className='imgInfo' style={stylesInfo.imgInfo}>
+            <img src={bandeja} alt='bandeja' />
+          </div>
+          <div className='infoText' style={stylesInfo.infoText}>
+          <p>La bandeja paisa es un plato típico de la gastronomía colombiana, originario de la región andina, específicamente de los departamentos de Antioquia y Caldas,​ y de la región paisa de Colombia, aunque su consumo se ha extendido a otras regiones del país.​</p>
+          </div>
+        </div>
       </div>
     </>
   )
