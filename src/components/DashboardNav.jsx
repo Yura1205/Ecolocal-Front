797d853fce716925d46203248c2abdb327bb63d2
Yura1205 <ledysy.torresr@unac.edu.co>
2023-11-10 04:33:23 -0500

    Styles :art:

--- src/components/DashboardNav.jsx
+++ src/components/DashboardNav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/logoWhite.png'
 import '../styles/navbar.css'
 import { NavLink } from 'react-router-dom'
 
