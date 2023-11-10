485eb185317ef2e8b27654d32e1686e5240bbe64
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2023-11-10 18:38:28 -0500

    Profiles new component

--- src/Pages/Profiles.jsx
+++ src/Pages/Profiles.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { DashboardSidebar } from '../components/DashboardSidebar'
 import { DashboardNav } from '../components/DashboardNav'
-
 import '../styles/profiles.css'
+import InfoUsers2 from '../components/InfoUsers2'
+
+
 export const Profiles = () => {
   return (
     <>
       <DashboardNav />
       <div className='profiles_main'>
         <DashboardSidebar />
+        <InfoUsers2 />
       </div>
 
     </>
---
+++ src/components/InfoUsers2.jsx
@@ -0,0 +1,119 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/esm/Button";
+import { MdDelete } from 'react-icons/md'
+import { FaUserEdit } from 'react-icons/fa'
+import ModalUsers from "./ModalUsers";
+import Constantes from "../utils/Constantes";
+
+const InfoUsers2 = () => {
+  const endPoint = Constantes.URL_SERVICIO + "/usuarios/listar";
+  const token = localStorage.getItem("token");
+  const [data, setData] = useState([]);
+  const MySwal = withReactContent(Swal);
+
+  const [selected, setSelected] = useState({});
+
+  // Modal
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = (elemento) => {
+    setShow(true)
+    setSelected(elemento);
+  };
+
+  const handleUsers = async () => {
+    await axios
+      .get(endPoint, {
+        headers: { Authorization: `bearer ${token}` },
+      })
+      .then((resp) => {
+        setData(resp.data.usuarios);
+      })
+      .catch((err) => {
+        console.log(err);
+        if (err.response.status === 400 || err.response.status === 401) {
+          Swal.fire("Información!", err.response.data.message, "error");
+        } else {
+          Swal.fire("Información!", "Ocurrió un error!", "error");
+        }
+      });
+  };
+
+  useEffect(() => {
+    handleUsers();
+  }, []);
+
+  const handleDelete = (element) => {
+    MySwal.fire({
+      title: `¿Está seguro de Eliminar a ${element.usuario}? Esta acción es irreversible!`,
+      showCancelButton: true,
+      confirmButtonText: "Sí",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        // Acción en caso de que elijan el Sí 
+        await axios
+          .delete(`${Constantes.URL_SERVICIO}/usuarios/eliminar/${element._id}`, {
+            headers: { Authorization: `bearer ${token}` },
+          })
+          .then((resp) => {
+            Swal.fire("Información!", resp.data.message, "success");
+            handleUsers();
+          })
+          .catch((err) => {
+            console.log(err);
+            if (err.response.status === 400 || err.response.status === 401) {
+              Swal.fire("Información!", err.response.data.message, "error");
+            } else {
+              Swal.fire("Información!", "Ocurrió un error!", "error");
+            }
+          });
+      }
+    });
+  };
+
+  // Filtrar la lista para mostrar solo el usuario autenticado
+  const usuarioAutenticado = data.find((user) => user.usuario === selected.usuario);
+
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombres</th>
+            <th>Apellidos</th>
+            <th>Email</th>
+            <th>Usuario</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usuarioAutenticado && (
+            <tr>
+              <td>1</td>
+              <td>{usuarioAutenticado.nombres}</td>
+              <td>{usuarioAutenticado.apellidos}</td>
+              <td>{usuarioAutenticado.email}</td>
+              <td>{usuarioAutenticado.usuario}</td>
+              <td>
+                <Button variant="primary" className="w-10 mr-1" onClick={() => handleShow(usuarioAutenticado)}>
+                  <FaUserEdit />
+                </Button>
+                <Button variant="danger" className="w-10" onClick={() => handleDelete(usuarioAutenticado)}>
+                  <MdDelete />
+                </Button>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+      <ModalUsers show={show} handleClose={handleClose} user={selected} getUser={handleUsers} />
+    </>
+  );
+};
+
+export default InfoUsers2;
