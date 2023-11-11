4dd99e49a26185895f5840ce9458093196e28961
DavidSanchez055 <davidf.sanchezm@una.edu.co>
2023-11-10 21:03:35 -0500

    profiles

--- src/Pages/Profiles.jsx
+++ src/Pages/Profiles.jsx
@@ -3,14 +3,14 @@ import { DashboardSidebar } from '../components/DashboardSidebar'
 import { DashboardNav } from '../components/DashboardNav'
 
 import '../styles/profiles.css'
-import InfoUser from '../components/InfoUser'
+import InfoUser2 from '../components/InfoUsers2'
 export const Profiles = () => {
   return (
     <>
       <DashboardNav />
       <div className='profiles_main'>
         <DashboardSidebar />
-        <InfoUser/>
+        <InfoUser2/>
       </div>
 
     </>
